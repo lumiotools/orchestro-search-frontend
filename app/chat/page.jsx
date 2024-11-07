0cf6da63e527f72108519800a4db95bdbe584691
DawoodKhatri <dawoodkhatri18@gmail.com>
2024-11-07 18:45:22 +0530

    Updated Chat Page

--- app/chat/page.jsx
+++ app/chat/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import ChatIntroInput from "@/components/chat/introInput";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
@@ -7,18 +6,27 @@ import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useToast } from "@/hooks/use-toast";
 import axios from "axios";
-import { LinkIcon, LoaderCircle, SendIcon, User, User2 } from "lucide-react";
-import Image from "next/image";
+import { LinkIcon, LoaderCircle, SendIcon, User2 } from "lucide-react";
 import React, { useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 const ChatPage = () => {
-  const [chatHistory, setChatHistory] = useState([]);
+  const [chatHistory, setChatHistory] = useState([
+    {
+      role: "assistant",
+      content: "Hello! How can I assist you with your shipping queries today?",
+    },
+  ]);
   const [question, setQuestion] = useState("");
   const [thinking, setThinking] = useState(false);
   const { toast } = useToast();
 
+  const inputRef = useRef(null);
   const scrollRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -83,23 +91,12 @@ const ChatPage = () => {
     }
   };
 
-  if (chatHistory.length === 0) {
-    return (
-      <ChatIntroInput
-        question={question}
-        setQuestion={setQuestion}
-        thinking={thinking}
-        handleAsk={handleAsk}
-        handleKeyPress={handleKeyPress}
-      />
-    );
-  }
   return (
-    <div className="max-w-screen-lg mx-auto h-screen p-8">
-      <Card className="flex flex-col h-full p-0 shadow-xl shadow-primary/5">
+    <div className="max-w-screen-lg mx-auto h-[calc(100vh-65px-64px)] pt-8">
+      <Card className="flex flex-col h-full p-0 shadow-xl shadow-primary/15">
         <ScrollArea className="flex-1 p-0 px-6" ref={scrollRef}>
           <div className="space-y-4 py-6">
-            {chatHistory.map(({ content, role, sources }, index) => (
+            {[...chatHistory].map(({ content, role, sources }, index) => (
               <div
                 key={index}
                 className={`flex ${
@@ -113,18 +110,14 @@ const ChatPage = () => {
                 >
                   {role === "assistant" ? (
                     <Avatar className="size-9">
-                      <AvatarImage
-                        src=""
-                        // src="https://github.com/polymet-ai.png"
-                        // alt="AI"
-                      />
+                      <AvatarImage src="" />
 
                       <AvatarFallback className="size-full bg-primary text-primary-foreground">
                         {role === "assistant" ? "AI" : "You"}
                       </AvatarFallback>
                     </Avatar>
                   ) : (
-                    <User2 className="size-9 p-2 bg-secondary text-primary rounded-full " />
+                    <User2 className="size-9 p-1.5 bg-secondary text-primary rounded-full " />
                   )}
 
                   <div className="space-y-2">
@@ -136,27 +129,26 @@ const ChatPage = () => {
                       }`}
                     >
                       <ReactMarkdown>{content}</ReactMarkdown>
-                      {/* <p className="text-sm">{content}</p> */}
                     </div>
 
                     {sources && sources.length > 0 && (
                       <div className="flex flex-wrap gap-2">
-                        {sources.map((source, index) => (
-                          <a
-                            key={index}
-                            href={source.url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center gap-1 text-xs text-amber-500 hover:text-amber-400"
-                          >
-                            {/* <img
-                              className="size-6"
-                              src={source.url + "/favicon.ico"}
-                            /> */}
-                            <LinkIcon className="h-3 w-3" />
-                            {source.url}
-                          </a>
-                        ))}
+                        {sources.map((source, index) =>
+                          source.score > 0.8 ? (
+                            <a
+                              key={index}
+                              href={source.url}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="flex items-center gap-1 text-xs text-amber-500 hover:text-amber-400"
+                            >
+                              <LinkIcon className="h-3 w-3" />
+                              {source.url}
+                            </a>
+                          ) : (
+                            <></>
+                          )
+                        )}
                       </div>
                     )}
                   </div>
@@ -166,7 +158,7 @@ const ChatPage = () => {
 
             {thinking && (
               <div className="flex justify-start gap-3">
-                <LoaderCircle className="animate-spin bg-white text-black rounded-full size-8 p-1" />
+                <LoaderCircle className="animate-spin bg-secondary text-primary rounded-full size-9 p-1.5" />
 
                 <div className="rounded-lg p-3 bg-secondary text-secondary-foreground">
                   <p className="text-sm">Thinking...</p>
@@ -179,6 +171,7 @@ const ChatPage = () => {
         <div className="p-4 border-t border-white/10">
           <div className="flex gap-2">
             <Input
+              ref={inputRef}
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
               onKeyPress={question && !thinking ? handleKeyPress : undefined}
