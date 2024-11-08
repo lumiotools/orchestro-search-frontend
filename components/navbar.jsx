5e11acb92b9def2902749bd5fee153104ddc752e
ritesh <rieshdhapatepatil@gmail.com>
2024-11-08 14:21:05 +0530

    final

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useContext } from "react";
 import { Button } from "@/components/ui/button";
 import { MessageSquare, BellIcon, UserCircleIcon } from "lucide-react";
@@ -6,31 +6,20 @@ import Link from "next/link";
 import { ChatContext } from "./chat/chatProvider";
 
 export function Navbar() {
-
-    const {setChatHistory} = useContext(ChatContext);
+  const { chatHistory, setChatHistory } = useContext(ChatContext);
 
   return (
-    <nav
-      className=" w-full z-50 custom-border-card border-golden-500/20 px-8"
-    >
-      <div className="container mx-auto px-4">
-        <div
-          className="flex items-center justify-between h-16"
-        >
-          <div
-            className="flex items-center space-x-8"
-          >
+    <nav className=" w-full z-50 custom-border-card border-golden-500/20 px-8">
+      <div className="max-w-screen-xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center space-x-8">
             <Link href={"/"}>
-            <h1
-              className="text-2xl font-bold bg-gradient-to-r from-amber-200 via-yellow-400 to-amber-500 bg-clip-text text-transparent cursor-pointer"
-            >
-              ShipTalk
-            </h1>
+              <h1 className="text-2xl font-bold bg-gradient-to-r from-amber-200 via-yellow-400 to-amber-500 bg-clip-text text-transparent cursor-pointer">
+                ShipTalk
+              </h1>
             </Link>
-            <div
-              className="hidden md:flex items-center space-x-6"
-            >
-                {/* <Link href={"/"}>
+            <div className="hidden md:flex items-center space-x-6">
+              {/* <Link href={"/"}>
               <Button
                 variant="ghost"
                 className="text-gray-300 hover:text-white"
@@ -42,24 +31,22 @@ export function Navbar() {
             </div>
           </div>
 
-          <div
-            className="flex items-center space-x-4"
-          >
-            <Button
-            onClick={()=>setChatHistory([])}
-              variant="ghost"
-              // size="icon"
-              className="relative"
-            >
-              <MessageSquare
-                className="h-5 w-5 text-gray-300"
-              />
-              New Chat
-              {/* <span
+          {chatHistory.length > 0 && (
+            <div className="flex items-center space-x-4">
+              <Button
+                onClick={() => setChatHistory([])}
+                variant="ghost"
+                // size="icon"
+                className="relative custom-secondary-card !bg-transparent hover:!bg-transparent"
+              >
+                <MessageSquare className="h-5 w-5 text-gray-300" />
+                New Chat
+                {/* <span
                 className="absolute -top-1 -right-1 h-3 w-3 bg-amber-500 rounded-full"
               /> */}
-            </Button>
-          </div>
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </nav>
